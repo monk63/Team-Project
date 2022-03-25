a7cfc1a2baa98951125829c6971efa72234acd8c
Mike Ofori <61565391+monk63@users.noreply.github.com>
2022-03-25 00:09:19 +0000

    f

--- Do It/lib/main.dart
+++ Do It/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'pages/Login_Screen.dart';
 import 'pages/splash.dart';
 
+//source: https://github.com/dragneel2074/login_logout_app/blob/master/lib/Screens/Home_Screen.dart
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
