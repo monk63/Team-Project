bdaa38459503d61f4f9724d87c3daf24b7610fcc
Mike Ofori <61565391+monk63@users.noreply.github.com>
2022-03-26 00:43:20 +0000

    gcgc

--- Do It/lib/pages/login_screen.dart
+++ Do It/lib/pages/login_screen.dart
@@ -20,6 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       body: isloading
           ? Center(
@@ -30,7 +31,10 @@ class _LoginScreenState extends State<LoginScreen> {
               child: AnnotatedRegion<SystemUiOverlayStyle>(
                 value: SystemUiOverlayStyle.light,
                 child: Stack(
+                  
                   children: [
+                    
+                    
                     Container(
                       height: double.infinity,
                       width: double.infinity,
@@ -41,11 +45,22 @@ class _LoginScreenState extends State<LoginScreen> {
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
+                            
+                            Container(
+                              
+                margin: EdgeInsets.only(top: 50),
+                child: Image.asset(
+                  "assets/images/splash.png",
+                  height: 90,
+                  width: 90,
+                ),
+                
+              ),
                             Text(
                               "Sign In",
                               style: TextStyle(
                                   fontSize: 50,
-                                  color: Colors.black,
+                                  color: Color.fromARGB(255, 42, 26, 190),
                                   fontWeight: FontWeight.bold),
                             ),
                             SizedBox(height: 30),
@@ -64,7 +79,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                 hintText: 'Email',
                                 prefixIcon: Icon(
                                   Icons.email,
-                                  color: Colors.black,
+                                  color: Color.fromARGB(255, 34, 44, 141),
                                 ),
                               ),
                             ),
@@ -84,12 +99,15 @@ class _LoginScreenState extends State<LoginScreen> {
                                   hintText: 'Password',
                                   prefixIcon: Icon(
                                     Icons.lock,
-                                    color: Colors.black,
+                                    color: Color.fromARGB(255, 50, 27, 153),
                                   )),
                             ),
                             SizedBox(height: 80),
+                            
                             LoginSignupButton(
+
                               title: 'Login',
+                              
                               ontapp: () async {
                                 if (formkey.currentState!.validate()) {
                                   setState(() {
@@ -146,7 +164,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                   Text(
                                     "Don't have an Account ?",
                                     style: TextStyle(
-                                        fontSize: 20, color: Colors.black87),
+                                        fontSize: 20, color: Color.fromARGB(221, 44, 53, 184)),
                                   ),
                                   SizedBox(width: 10),
                                   Hero(
@@ -156,7 +174,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                       style: TextStyle(
                                           fontSize: 21,
                                           fontWeight: FontWeight.bold,
-                                          color: Colors.black),
+                                          color: Color.fromARGB(255, 182, 89, 3)),
                                     ),
                                   )
                                 ],
@@ -171,193 +189,6 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
     );
+    
   }
 }
-
-// import 'package:flutter/material.dart';
-
-// import 'goals_screen.dart';
-// import 'signup_screen.dart';
-
-// class LoginScreen extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() => StartState();
-// }
-
-// class StartState extends State<LoginScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return initWidget();
-//   }
-
-//   initWidget() {
-//     var container = Container(
-//             alignment: Alignment.center,
-//             margin: EdgeInsets.only(left: 20, right: 20, top: 70),
-//             padding: EdgeInsets.only(left: 20, right: 20),
-//             height: 54,
-//             decoration: BoxDecoration(
-//               gradient: LinearGradient(
-//                   colors: [(new Color(0xffF5591F)), new Color(0xffF2861E)],
-//                   begin: Alignment.centerLeft,
-//                   end: Alignment.centerRight),
-//               borderRadius: BorderRadius.circular(50),
-//               color: Colors.grey[200],
-//               boxShadow: [
-//                 BoxShadow(
-//                     offset: Offset(0, 10),
-//                     blurRadius: 50,
-//                     color: Color(0xffEEEEEE)),
-//               ],
-//             ),
-//             child: Text(
-//               "LOGIN",
-//               style: TextStyle(color: Colors.white),
-//             ),
-           
-//         );
-//     return Scaffold(
-//         body: SingleChildScrollView(
-//             child: Column(
-//       children: [
-//         Container(
-//           height: 300,
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.only(bottomLeft: Radius.circular(90)),
-//             color: new Color(0xffF5591F),
-//             gradient: LinearGradient(
-//               colors: [(new Color(0xffF5591F)), new Color(0xffF2861E)],
-//               begin: Alignment.topCenter,
-//               end: Alignment.bottomCenter,
-//             ),
-//           ),
-//           child: Center(
-//               child: Column(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//               Container(
-//                 margin: EdgeInsets.only(top: 50),
-//                 child: Image.asset(
-//                   "assets/images/splash.png",
-//                   height: 90,
-//                   width: 90,
-//                 ),
-//               ),
-//               Container(
-//                 margin: EdgeInsets.only(right: 20, top: 20),
-//                 alignment: Alignment.bottomRight,
-//                 child: Text(
-//                   "Login",
-//                   style: TextStyle(fontSize: 20, color: Colors.white),
-//                 ),
-//               )
-//             ],
-//           )),
-//         ),
-//         Container(
-//           alignment: Alignment.center,
-//           margin: EdgeInsets.only(left: 20, right: 20, top: 70),
-//           padding: EdgeInsets.only(left: 20, right: 20),
-//           height: 54,
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(50),
-//             color: Colors.grey[200],
-//             boxShadow: [
-//               BoxShadow(
-//                   offset: Offset(0, 10),
-//                   blurRadius: 50,
-//                   color: Color(0xffEEEEEE)),
-//             ],
-//           ),
-//           child: TextField(
-//             cursorColor: Color(0xffF5591F),
-//             decoration: InputDecoration(
-//               icon: Icon(
-//                 Icons.email,
-//                 color: Color(0xffF5591F),
-//               ),
-//               hintText: "Enter Email",
-//               enabledBorder: InputBorder.none,
-//               focusedBorder: InputBorder.none,
-//             ),
-//           ),
-//         ),
-//         Container(
-//           alignment: Alignment.center,
-//           margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-//           padding: EdgeInsets.only(left: 20, right: 20),
-//           height: 54,
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(50),
-//             color: Color(0xffEEEEEE),
-//             boxShadow: [
-//               BoxShadow(
-//                   offset: Offset(0, 20),
-//                   blurRadius: 100,
-//                   color: Color(0xffEEEEEE)),
-//             ],
-//           ),
-//           child: TextField(
-//             cursorColor: Color(0xffF5591F),
-//             decoration: InputDecoration(
-//               focusColor: Color(0xffF5591F),
-//               icon: Icon(
-//                 Icons.vpn_key,
-//                 color: Color(0xffF5591F),
-//               ),
-//               hintText: "Enter Password",
-//               enabledBorder: InputBorder.none,
-//               focusedBorder: InputBorder.none,
-//             ),
-//           ),
-//         ),
-//         Container(
-//           margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-//           alignment: Alignment.centerRight,
-//           child: GestureDetector(
-//             onTap: () {
-//               // Write Click Listener Code Here
-//             },
-//             child: Text("Forget Password?"),
-//           ),
-//         ),
-//         GestureDetector(
-//           onTap: () {
-//               onPressed: (){
-//               Navigator.push(
-//                 context,
-//                 MaterialPageRoute(builder: (context)=> sec() ),
-//               );
-            
-//             // Write Click Listener Code Here.
-//           };
-//           },
-//           child: container,),
-//         Container(
-//           margin: EdgeInsets.only(top: 10),
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Text("Don't Have Any Account?  "),
-//               GestureDetector(
-//                 child: Text(
-//                   "Register Now",
-//                   style: TextStyle(color: Color(0xffF5591F)),
-//                 ),
-//                 onTap: () {
-//                   // Write Tap Code Here.
-//                   Navigator.push(
-//                       context,
-//                       MaterialPageRoute(
-//                         builder: (context) => SignUpScreen(),
-//                       ));
-//                 },
-//               )
-//             ],
-//           ),
-//         )
-//       ],
-//     )));
-//   }
-// }
