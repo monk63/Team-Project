31dc199263032c66284f67f203a863f078570494
Mike Ofori <61565391+monk63@users.noreply.github.com>
2022-04-24 22:02:39 +0000

    c c c

---
+++ Do It/lib/Component/constants.dart
@@ -0,0 +1,7 @@
+const String appName = 'com.example.riverpod_flutter_todo_app';
+const String fbUrl = 'https://www.facebook.com';
+const String githubUrl = 'https://github.com/monk63/Team-Project/tree/main/Do%20It';
+const String mailUrl = 'mailto:mail@example.com?subject=&body=';
+const String sharedPrefTodoListKey = 'SharedData';
+const String sharedDarkModeKey = 'isDarkModeEnabled';
+const String emptyJsonStringData = '{"data": []}';
